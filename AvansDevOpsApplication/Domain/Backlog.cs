5a8bbdc5f099880252e2abff701c56c96c11cf3a
Tristan <tristandevelop@protonmail.com>
2024-03-18 13:57:35 +0100

    started on backlog

--- AvansDevOpsApplication/Domain/Backlog.cs
+++ AvansDevOpsApplication/Domain/Backlog.cs
@@ -2,6 +2,34 @@
 {
     public class Backlog
     {
+        private Guid id;
         private List<BacklogItem> backlogItems;
+        private List<Sprint> sprints;
+
+        public Backlog()
+        {
+            backlogItems = [];
+            id = Guid.NewGuid();
+        }
+
+        public void AddBacklogItem(BacklogItem backlogItem)
+        {
+
+        }
+
+        public void RemoveBacklogItem(Guid id)
+        {
+
+        }
+
+        public void AddSprint(Sprint sprint)
+        {
+            sprints.Add(sprint);
+        }
+
+        public void RemoveSprint(Guid id)
+        {
+            sprints.RemoveAll(x => x.Id == id);
+        }
     }
 }
---
+++ AvansDevOpsApplication/Domain/Sprint.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvansDevOpsApplication.Domain
+{
+
+    public abstract class Sprint
+    {
+        private Guid id;
+        private string name;
+
+        public Sprint(Guid id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public Guid Id
+        {
+            get { return id; }
+        }
+    }
+}
