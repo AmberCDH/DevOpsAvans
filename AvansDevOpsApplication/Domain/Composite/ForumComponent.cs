1e98306430bcfa0a4bf6928853ac0a5864ddf9e4
AmberCDH <kanarieamber@gmail.com>
2024-03-18 17:28:37 +0100

    Composite pattern Forum

---
+++ AvansDevOpsApplication/Domain/Composite/ForumComponent.cs
@@ -0,0 +1,23 @@
+﻿namespace AvansDevOpsApplication.Domain.Composite
+{
+    // gebruiker kan op een forum dingen posten 
+    public abstract class ForumComponent
+    {
+        public virtual void Add(ForumComponent forumComponent) 
+        {
+            throw new NotImplementedException();
+        }
+        public virtual void Remove(ForumComponent forumComponent)
+        {
+            throw new NotImplementedException();
+        }
+        public virtual ForumComponent GetChild(int i)
+        {
+            throw new NotImplementedException();
+        }
+
+        public abstract void Print();
+
+
+    }
+}
