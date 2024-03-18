7050f5e492160af9e529a4a2fdd42a73373bb799
AmberCDH <kanarieamber@gmail.com>
2024-03-18 13:04:56 +0100

    BacklogItems added getters and setters

--- AvansDevOpsApplication/Domain/BacklogItem.cs
+++ AvansDevOpsApplication/Domain/BacklogItem.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Xml.Linq;
+
 namespace AvansDevOpsApplication.Domain
 {
     public class BacklogItem
@@ -10,7 +12,33 @@ namespace AvansDevOpsApplication.Domain
         private bool wantsNotification;
         private string inList;
         private DateTime timeOfCreation;
-        public BacklogItem() { }
+        public BacklogItem(string name, string description, List<Activity>? activitys, DateTime timeOfCreation, string inList)
+        {
+            this.name = name;
+            this.description = description;
+            this.timeOfCreation = timeOfCreation;
+            this.inList = inList;
+            this.activitys = activitys ?? new List<Activity>();
+        }
 
+        public string Name { get => name; set => name = value; }
+        public string Description { get => description; set => description = value; }
+        public User User { get => user; set => user = value; }
+        public bool WantsNotification { get => wantsNotification; set => wantsNotification = value; }
+        public string InList { get => inList; set => inList = value; }
+        public DateTime TimeOfCreation { get => timeOfCreation; set => timeOfCreation = value; }
+        public List<Activity> Activitys { get => activitys; set => activitys = value; }
+        public void AddActivityToList(Activity activity)
+        {
+            activitys.Add(activity);
+        }
+
+        // Alleen als een user eraan gekoppeld is, kan deze verplaatst worden,
+        // notificaties geabboneerd en uit de backlog komen(inList van een een ander komen).
+        public string toString()
+        {
+            return "BacklogItem ~ Name; " + name + "; description ~ " + description + "; timeOfCreation ~ " + timeOfCreation + "; Activity's ~ " + activitys.Count;
+
+        }
     }
 }
