788d10674d1ebfd2949cb6c8ac13c81b7fc00192
AmberCDH <kanarieamber@gmail.com>
2024-03-19 19:08:38 +0100

    Can make a pdf with footer and header

--- AvansDevOpsApplication/Domain/Factory/ExportPdf.cs
+++ AvansDevOpsApplication/Domain/Factory/ExportPdf.cs
@@ -1,12 +1,39 @@
-﻿
+﻿using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Previewer;
+
 namespace AvansDevOpsApplication.Domain.Factory
 {
     public class ExportPdf : Export
     {
+        [Obsolete]
         public void exportReport(string header, string footer, List<User> teamInSprint)
         {
             Console.WriteLine("Pdf");
-            //throw new NotImplementedException();
+            Document.Create(container =>
+            {
+                container.Page(page =>
+                {
+                    page.Size(PageSizes.A4);
+                    page.Header().Height(100).Background(Colors.Grey.Lighten1).AlignCenter().Text(text =>
+                    {
+                        text.DefaultTextStyle(x => x.Size(20));
+                        text.Span($"{header}");
+                        text.Element()
+                        .PaddingBottom(-6)
+                        .Height(24)
+                        .Width(48)
+                        .Image(Placeholders.Image);
+                    });
+                    page.Content().Background(Colors.Grey.Lighten3);
+                    page.Footer().Height(50).Background(Colors.Grey.Lighten1).AlignCenter().Text(text =>
+                    {
+                        text.DefaultTextStyle(x => x.Size(16));
+                        text.Span($"{footer}");
+                    });
+                });
+            })
+                .ShowInPreviewer();
         }
     }
 }
--- AvansDevOpsApplication/Program.cs
+++ AvansDevOpsApplication/Program.cs
@@ -3,37 +3,49 @@ using AvansDevOpsApplication.Domain;
 using AvansDevOpsApplication.Domain.Composite;
 using AvansDevOpsApplication.Domain.Factory;
 
-var localDate = DateTime.Now;
-var birthday = new DateOnly(2000, 1, 12);
-var user = new User("Tom", "t@mail.com", 24, birthday, RoleType.LEAD_DEVELOPER);
-Console.WriteLine(user.toString());
-user.Name = "Test";
-Console.WriteLine(user.toString());
-user.Role = RoleType.SCRUM_MASTER;
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        var localDate = DateTime.Now;
+        var birthday = new DateOnly(2000, 1, 12);
+        var user = new User("Tom", "t@mail.com", 24, birthday, RoleType.LEAD_DEVELOPER);
+        Console.WriteLine(user.toString());
+        QuestPDF.Settings.License = QuestPDF.Infrastructure.LicenseType.Community;
 
-Console.WriteLine(user.toString());
+        user.Name = "Test";
+        Console.WriteLine(user.toString());
+        user.Role = RoleType.SCRUM_MASTER;
 
-var activity = new Activity("help", "done", "not done", localDate);
-Console.WriteLine(activity.toString());
+        Console.WriteLine(user.toString());
 
-var backlogItem = new BacklogItem("Als gebruiker...", "Nice description", null, localDate, "Backlog");
-Console.WriteLine(backlogItem.toString());
-backlogItem.AddActivityToList(activity);
-Console.WriteLine(backlogItem.toString());
+        var activity = new Activity("help", "done", "not done", localDate);
+        Console.WriteLine(activity.toString());
 
-// Composite
-List<ForumComponent> reactionsUS1 = [new Reactions("Niemand gebruikt dit", localDate, user)];
-List<ForumComponent> reactionsUS2 = [new Reactions("Ik ga huilen hoor", localDate, user), new Reactions("...", localDate, user)];
+        var backlogItem = new BacklogItem("Als gebruiker...", "Nice description", null, localDate, "Backlog");
+        Console.WriteLine(backlogItem.toString());
+        backlogItem.AddActivityToList(activity);
+        Console.WriteLine(backlogItem.toString());
 
-ForumComponent FirstUserStory = new Forum(backlogItem);
-FirstUserStory.Add(new Post("Waar is de rest van dit item?", "US is onduidelijk", localDate, user, reactionsUS1));
+        // Composite
+        List<ForumComponent> reactionsUS1 = [new Reactions("Niemand gebruikt dit", localDate, user)];
+        List<ForumComponent> reactionsUS2 = [new Reactions("Ik ga huilen hoor", localDate, user), new Reactions("...", localDate, user)];
 
-FirstUserStory.Add(new Post("Help?", "US is onduidelijk", localDate, user, reactionsUS2));
+        ForumComponent FirstUserStory = new Forum(backlogItem);
+        FirstUserStory.Add(new Post("Waar is de rest van dit item?", "US is onduidelijk", localDate, user, reactionsUS1));
 
-FirstUserStory.Print(); 
+        FirstUserStory.Add(new Post("Help?", "US is onduidelijk", localDate, user, reactionsUS2));
 
-// Factory export
-Report report = new Report();
-Console.WriteLine(report.createExport("pdf"));
-Console.WriteLine(report.createExport("png"));
-Console.WriteLine(report.createExport(""));
+        FirstUserStory.Print();
+
+        // Factory export1
+        List<User> users = new List<User>();
+        users.Add(user);
+        Report report = new Report();
+        report.createExport("pdf").exportReport("hey", "bye", users);
+        Console.WriteLine(report.createExport("png"));
+        Console.WriteLine(report.createExport(""));
+
+        Console.ReadLine();
+    }
+}
