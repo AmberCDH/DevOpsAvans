fb96353fbf4ccbbb489d3a401ad8af148ba2f485
Tristan <tristandevelop@protonmail.com>
2024-03-21 14:26:54 +0100

    Moved to adapter folder

---
+++ AvansDevOpsApplication/Domain/Adapter/EmailObserver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AvansDevOpsApplication.Domain.Observer;
+
+namespace AvansDevOpsApplication.Domain.Adapter
+{
+    internal class EmailObserver : IObserver
+    {
+        public void Update(string message)
+        {
+            Console.WriteLine("Email: " + message);
+        }
+    }
+}
